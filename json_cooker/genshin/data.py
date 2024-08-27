59eabd7b898187bb83852f84531ea4529cce613f
seriaati <seria.ati@gmail.com>
2024-08-27 11:28:07 +0800

    fix: Change enka api docs data path

--- json_cooker/genshin/data.py
+++ json_cooker/genshin/data.py
@@ -18,13 +18,13 @@ LANGS: Final[dict[str, str]] = {
     "VI": "vi",
 }
 ENKA_API_DOCS: Final[str] = (
-    "https://raw.githubusercontent.com/EnkaNetwork/API-docs/master"
+    "https://raw.githubusercontent.com/pizza-studio/EnkaDBGenerator/main/Sources/EnkaDBFiles/Resources/Specimen/GI"
 )
 ANIME_GAME_DATA: Final[str] = "https://gitlab.com/Dimbreath/AnimeGameData/-/raw/master"
 
-LOC_JSON: Final[str] = f"{ENKA_API_DOCS}/store/loc.json"
-NAMECARDS: Final[str] = f"{ENKA_API_DOCS}/store/namecards.json"
-CHARACTERS: Final[str] = f"{ENKA_API_DOCS}/store/characters.json"
+LOC_JSON: Final[str] = f"{ENKA_API_DOCS}/loc.json"
+NAMECARDS: Final[str] = f"{ENKA_API_DOCS}/namecards.json"
+CHARACTERS: Final[str] = f"{ENKA_API_DOCS}/characters.json"
 
 ARTIFACTS: Final[str] = (
     f"{ANIME_GAME_DATA}/ExcelBinOutput/ReliquaryExcelConfigData.json"
