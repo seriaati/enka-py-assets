86ebc1355eae1268f38141860112c9a02f79aa96
seria <seria.ati@gmail.com>
2024-10-23 07:45:17 +0900

    fix: I'm stupid

--- json_cooker/hsr/data.py
+++ json_cooker/hsr/data.py
@@ -1,4 +1,4 @@
-STARRAIL_DATA = "https://gitlab.com/Dimbreath/turnbasedgamedata"
+STARRAIL_DATA = "https://gitlab.com/Dimbreath/turnbasedgamedata/-/raw/main"
 
 SKILL_TREE = f"{STARRAIL_DATA}/ExcelOutput/AvatarSkillTreeConfig.json"
 PROPERTY_CONFIG = f"{STARRAIL_DATA}/ExcelOutput/AvatarPropertyConfig.json"
