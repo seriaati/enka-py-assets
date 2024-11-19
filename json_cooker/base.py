9f0832ddef4b5592849dc4e01ec98d0c103fdcb0
seria <seria.ati@gmail.com>
2024-11-19 11:36:00 +0800

    imp: Add error handling in file download and save

--- json_cooker/base.py
+++ json_cooker/base.py
@@ -15,11 +15,19 @@ class JSONCooker:
 
     async def _download(self, url: str, name: str) -> None:
         LOGGER_.info("Downloading %s from %s", name, url)
-        async with self._session.get(url) as resp:
-            self._data[name] = orjson.loads(await resp.text(encoding="utf-8"))
+        try:
+            async with self._session.get(url) as resp:
+                self._data[name] = orjson.loads(await resp.text(encoding="utf-8"))
+        except Exception as e:
+            LOGGER_.error("Failed to download %s: %s", name, e)
+            raise e
 
     async def _save_data(self, name: str, data: Any) -> None:
         LOGGER_.info("Saving %s.json...", name)
-        async with aiofiles.open(f"data/{name}.json", "w", encoding="utf-8") as f:
-            bytes_ = orjson.dumps(data)
-            await f.write(bytes_.decode())
+        try:
+            async with aiofiles.open(f"data/{name}.json", "w", encoding="utf-8") as f:
+                bytes_ = orjson.dumps(data)
+                await f.write(bytes_.decode())
+        except Exception as e:
+            LOGGER_.error("Failed to save %s: %s", name, e)
+            raise e
