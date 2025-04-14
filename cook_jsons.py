7b67e5f275c5cb9f8883c13ef9cb53b2939e379c
seria <seria.ati@gmail.com>
2025-04-14 18:00:20 +0900

    feat: Add CLI args for cooker

--- cook_jsons.py
+++ cook_jsons.py
@@ -1,11 +1,35 @@
 import asyncio
 import logging
+import argparse
 
 import aiohttp
 
 from json_cooker.genshin.cooker import GenshinJSONCooker
 from json_cooker.hsr.cooker import HSRJSONCooker
 from json_cooker.zzz.cooker import ZZZJSONCooker
+
+arg_parser = argparse.ArgumentParser(
+    description="Cook JSON files for Genshin, HSR, and ZZZ."
+)
+arg_parser.add_argument(
+    "--gi",
+    action="store_true",
+    help="Cook Genshin JSON files.",
+    default=False,
+)
+arg_parser.add_argument(
+    "--hsr",
+    action="store_true",
+    help="Cook HSR JSON files.",
+    default=False,
+)
+arg_parser.add_argument(
+    "--zzz",
+    action="store_true",
+    help="Cook ZZZ JSON files.",
+    default=False,
+)
+args = arg_parser.parse_args()
 
 
 async def main() -> None:
@@ -18,14 +42,17 @@ async def main() -> None:
     logging.basicConfig(level=logging.INFO, handlers=[handler])
 
     async with aiohttp.ClientSession() as session:
-        # cooker = GenshinJSONCooker(session)
-        # await cooker.cook()
+        if args.gi:
+            cooker = GenshinJSONCooker(session)
+            await cooker.cook()
 
-        # cooker = HSRJSONCooker(session)
-        # await cooker.cook()
+        if args.hsr:
+            cooker = HSRJSONCooker(session)
+            await cooker.cook()
 
-        cooker = ZZZJSONCooker(session)
-        await cooker.cook()
+        if args.zzz:
+            cooker = ZZZJSONCooker(session)
+            await cooker.cook()
 
 
 asyncio.run(main())
