1f8f67b6e99914c405237f0f388044be2c326e2c
seria <seria.ati@gmail.com>
2025-10-22 16:20:21 +0900

    fix: Use Enka API Docs instead of pizza helper

--- json_cooker/genshin/data.py
+++ json_cooker/genshin/data.py
@@ -16,12 +16,14 @@ LANGS = {
     "TR": "tr",
     "VI": "vi",
 }
-ENKA_API_DOCS = "https://raw.githubusercontent.com/pizza-studio/EnkaDBGenerator/main/Sources/EnkaDBFiles/Resources/Specimen/GI"
+ENKA_API_DOCS = (
+    "https://raw.githubusercontent.com/EnkaNetwork/API-docs/refs/heads/master/store"
+)
 ANIME_GAME_DATA = "https://gitlab.com/Dimbreath/AnimeGameData/-/raw/master"
 
 LOC_JSON = f"{ENKA_API_DOCS}/loc.json"
-NAMECARDS = f"{ENKA_API_DOCS}/namecards.json"
-CHARACTERS = f"{ENKA_API_DOCS}/characters.json"
+NAMECARDS = f"{ENKA_API_DOCS}/gi/namecards.json"
+CHARACTERS = f"{ENKA_API_DOCS}/gi/avatars.json"
 
 ARTIFACTS = f"{ANIME_GAME_DATA}/ExcelBinOutput/ReliquaryExcelConfigData.json"
 TEXT_MAP = f"{ANIME_GAME_DATA}/TextMap/TextMap{{lang}}.json"
